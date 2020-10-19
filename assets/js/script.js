d5dbd4765cba3e58ad9bdd289800492ad3a28c31
Jason Jimenez <jasonjimenez@MacBook-Pro.lan>
2020-10-19 13:25:17 -0500

    4.2.8

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,16 @@ var taskFormHandler = function(event) {
     event.preventDefault(); //keeps task from disappearing/refreshing
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
-    var taskTypeInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput
     };
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    formEl.reset();
     createTaskEl(taskDataObj);
     
     //listItemEl.textContent = taskNameInput,taskTypeInput  
